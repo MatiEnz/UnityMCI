6b2dc95bb95a3ef5228529ee9166d99464d35c5c
Mateus Enzenberg <mati.anonym@hotmail.com>
2021-11-12 19:57:16 +0100

    Added infected spawn and particles
    Basic game play

--- UnityProject2021/Assets/Scripts/SpawnInfected.cs
+++ UnityProject2021/Assets/Scripts/SpawnInfected.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnInfected : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+ public class SpawnInfected: MonoBehaviour {
+ 
+ public float targetTime = 3.0f;
+ 
+ public GameObject objectToSpawn;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+ void Update(){
+ 
+ targetTime -= Time.deltaTime;
+ 
+ if (targetTime <= 0.0f)
+ {
+    timerEnded();
+ }
+ 
+ }
+ 
+ void timerEnded()
+ {
+    Instantiate(objectToSpawn, new Vector3(Random.Range(-1.5f,1.5f),0.0f,Random.Range(2.0f,2.5f)), Quaternion.identity);
+    targetTime = Random.Range(1.0f,10.0f);
+ }
+ 
+ 
+ }
