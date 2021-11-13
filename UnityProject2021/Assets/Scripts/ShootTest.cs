06eec985bd5de49f52f10e32d86080ca6e7dddd4
Mateus Enzenberg <mati.anonym@hotmail.com>
2021-11-13 22:32:54 +0100

    Tweaked Needle Physics
    Gravity too strong, use constant force is nicer for projectile arc

--- UnityProject2021/Assets/Scripts/ShootTest.cs
+++ UnityProject2021/Assets/Scripts/ShootTest.cs
@@ -9,14 +9,17 @@ public class ShootTest : MonoBehaviour
     public GameObject projectile;
 
     public float shootForce = 500.0f;
-
+    public float shootUp = 20.0f;
+    private Vector3 projectileOrigin;
 
     void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
-            GameObject bullet = Instantiate(projectile, arCamera.position, arCamera.rotation) as GameObject;
+            projectileOrigin = new Vector3(arCamera.position.x, arCamera.position.y-0.1f, arCamera.position.z-0.05f);
+            GameObject bullet = Instantiate(projectile, projectileOrigin, arCamera.rotation) as GameObject;
             bullet.GetComponent<Rigidbody>().AddForce(arCamera.forward * shootForce);
+            bullet.GetComponent<Rigidbody>().AddForce(arCamera.up * shootUp);
         }
     }
 }
